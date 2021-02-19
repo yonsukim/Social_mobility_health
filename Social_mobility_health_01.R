be7794d6da38b4f818ca90ee267cb0a6495a8a2c
Yonsu <yonsuk@uci.edu>
2021-02-19 09:31:30 -0800

    Get started

--- Social_mobility_health_01.R
+++ Social_mobility_health_01.R
@@ -1,2 +1,13 @@
-test..
-test 1 
+
+#####  Social mobility and self-rated health
+#####  YKim Created - 2/19/21
+
+install.packages("readstata13")
+install.packages("rlang", dependencies = TRUE)
+install.packages("readxl")
+install.packages("tidyverse")
+
+library(readstata13)
+library("readxl")
+# library(readxl)
+library(dplyr)
